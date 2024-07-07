2c4b48aef48a4c93b1606aec700b6943171d736a
Josef Vasata <vasatjos@fit.cvut.cz>
2024-07-07 17:00:57 +0200

    Add README and edit install script

---
+++ README.md
@@ -0,0 +1,6 @@
+# Prerequisites 
+- Have Hack Nerd Font installed
+- Have Alacritty installed
+- Have Oh My Zsh installed
+- Have Tmux installed
+- Have Neovim installed
--- install.sh
+++ install.sh
@@ -1,11 +1,27 @@
 #/bin/bash
 
-# Install tpm
-if [ -d ~/.tmux/plugins/tpm ]; then
+# install tpm
+if [ -d $HOME/.tmux/plugins/tpm ]; then
     echo "tpm already installed"
 else
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    echo "Installing tpm"
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
+
+# install tmux plugins
+echo "Installing tmux plugins"
+tmux start-server
+tmux new-session -d
+$HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
+tmux kill-server
 
 # clone nvim submodule
 git submodule update --init --recursive
+
+# install p10k
+if [ -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]; then
+    echo "p10k already installed"
+else
+    echo "Installing p10k"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
