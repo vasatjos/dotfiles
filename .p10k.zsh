0256deba3a11de03acb1d40f6c3ec1358ee0f7a9
Josef Vasata <jvasata03@gmail.com>
2025-10-12 16:37:30 +0200

    Add git icon

--- .p10k.zsh
+++ .p10k.zsh
@@ -348,7 +348,7 @@
 
   #####################################[ vcs: git status ]######################################
   # Branch icon. Set this parameter to '\UE0A0 ' for the popular Powerline branch icon.
-  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=
+  typeset -g POWERLEVEL9K_VCS_BRANCH_ICON='󰊢 '
 
   # Untracked files icon. It's really a question mark, your font isn't broken.
   # Change the value of this parameter to show a different icon.
